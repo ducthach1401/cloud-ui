9cbd7273b59ece80a18b4e90c212fa1be9e26159
Đức Thạch <ducthach.1401@gmail.com>
2023-01-13 11:33:09 +0700

    update version 0.0.3

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npm run build
-docker build --pull --rm -f "dockerfile" -t ducthach1401/cloud-ui:0.0.2 "."
-docker push ducthach1401/cloud-ui:0.0.2
+docker build --pull --rm -f "dockerfile" -t ducthach1401/cloud-ui:0.0.3 "."
+docker push ducthach1401/cloud-ui:0.0.3
